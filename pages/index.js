3f204ee846fddf01cb0e6f98484104658bc36331
Mai An <81462692+anmq0502@users.noreply.github.com>
2024-02-02 05:05:17 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+var mysql = require('mysql');
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 export default function Home() {
   return (
     <div className="container">
